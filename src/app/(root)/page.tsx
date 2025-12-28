38d3a806b60e29acb381df5a20c8404cb1a4e1d7
Rendles <romulintus@gmail.com>
2025-12-28 14:48:48 +0700

    feat(ContactPage): reworked contact page (in progress)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,5 @@
-import ContactForm from "@/components/ContactForm";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles["main-page-layout"]}>
-      <ContactForm />
-    </div>
-  );
+  return <div className={styles["main-page-layout"]}></div>;
 }
--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+import ContactForm from "@/components/ContactForm";
+import styles from "./page.module.css";
 
+export default function Home() {
   return (
-    <div>
-      <span>Текст какой-то</span>
+    <div className={styles["contact-page"]}>
+      <div className={styles["contact-page-info"]}>
+        <h1>Contact Us</h1>
+        <p>
+          If you have any questions, feel free to reach out to us using the form
+          below.
+        </p>
+      </div>
+      <div className={styles["contact-page-form-wrapper"]}>
+        <ContactForm />
+      </div>
     </div>
   );
 }
